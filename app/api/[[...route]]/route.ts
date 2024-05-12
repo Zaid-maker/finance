4cb4b0daead8855ea04607b77b529267136cad56
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-12 12:31:47 +0000

    test route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -10,6 +10,14 @@ app.get('/hello', (c) => {
         message: 'Hello Next.js!',
     })
 })
+    .get('/hello/:test', (c) => {
+        const test = c.req.param('test')
+
+        return c.json({
+            message: 'Hello world this is test',
+            test: test
+        })
+    })
 
 export const GET = handle(app)
 export const POST = handle(app)
