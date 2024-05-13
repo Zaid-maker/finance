42d14c90e84bd3645d6010587e13fecc8dc72f93
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-13 10:56:06 +0000

    define routes for navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,6 +2,35 @@
 
 import React from "react";
 
+const routes = [
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+  {
+    href: "/accounts",
+    label: "Accounts",
+  },
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
+
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <p>{route.label}</p>
+      ))}
+    </nav>
+  );
 };
