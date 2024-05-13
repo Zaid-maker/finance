cdd6a4797879cd9bc286ede416bba3eda74d6f17
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-13 10:41:25 +0000

    HeaderLogo

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { HeaderLogo } from "./header-logo";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            <HeaderLogo />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
