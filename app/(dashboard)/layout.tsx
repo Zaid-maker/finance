067459bcb0e76cef405b3ec493e4f55683080f9e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-13 10:20:17 +0000

    header component and dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/header";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <div className="px-3 lg:px-14">{children}</div>;
+    </>
+  );
+};
+
+export default DashboardLayout;
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,6 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return <UserButton />;
+}
