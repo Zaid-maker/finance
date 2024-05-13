999bbc45fb0b57ba7c74f5d0fd0cc913192ebdd2
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-13 10:11:23 +0000

    check if user is authenticated or not

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,23 +1,22 @@
-import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-    .get("/hello", (c) => {
-        return c.json({
-            message: "Hello Next.js!",
-        });
-    })
-    .get("/hello/:test", (c) => {
-        return c.json({
-            message: "Hello world this is test",
-        });
+app.get("/hello", clerkMiddleware(), (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" });
+    }
+    return c.json({
+        message: "Hello Next.js!",
+        userId: auth.userId
     });
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
