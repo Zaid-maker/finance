f5f2f129ab5ac6f4d820f3ee075a38af6d005de9
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-05-14 08:22:15 +0000

    Style Nav Buttons

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 type Props = {
   href: string;
@@ -13,7 +14,10 @@ export const NavButton = ({ href, label, isActive }: Props) => {
       asChild
       size="sm"
       variant="outline"
-      className={isActive ? "ss" : "ss"}
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transtion",
+        isActive ? "bg-white/10 text-white" : "bg-transparent"
+      )}
     >
       <Link href={href}>{label}</Link>
     </Button>
