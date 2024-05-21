79efc61ef36d9bd6ecfb41dc00d100673fe3172b
DevMirza <pzhafeez@gmail.com>
2024-05-21 17:22:34 +0000

    test

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,8 +2,16 @@ import { Hono } from "hono";
 
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "@hono/http-exception";
 
-const app = new Hono().get("/", async (c) => {
+const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    throw new HTTPExeption();
+  }
+
   const data = await db
     .select({
       id: accounts.id,
