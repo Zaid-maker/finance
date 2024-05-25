2d6f5ae89834a92df6b0b21401bbb6634b8bb026
Author <actions@github.com>
2024-05-25 08:20:38 +0000

    [ci] Format changes

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -32,9 +32,13 @@ export const NewAccountSheet = () => {
           <SheetTitle>New Account</SheetTitle>
           <SheetDescription>Create a new account to track your transactions.</SheetDescription>
         </SheetHeader>
-        <AccountForm onSubmit={onSubmit} disabled={false} defaultValues={{
-          name: ''
-        }} />
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={false}
+          defaultValues={{
+            name: '',
+          }}
+        />
       </SheetContent>
     </Sheet>
   );
