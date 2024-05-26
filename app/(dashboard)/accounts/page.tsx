6357f3fbf5651f4c8a8677a69af95fdc34e0b6b8
Author <actions@github.com>
2024-05-26 10:39:47 +0000

    [ci] Format changes

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,29 +1,27 @@
-'use client'
+'use client';
 
-import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
-import { Plus } from 'lucide-react'
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+import { Plus } from 'lucide-react';
+import React from 'react';
 
 const AccountsPage = () => {
-  const newAccount = useNewAccount()
+  const newAccount = useNewAccount();
 
   return (
     <div>
-      <Card className='border-none drop-shadow-sm'>
-        <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
-          <CardTitle className='text-xl line-clamp-1'>
-            Accounts Page
-          </CardTitle>
-          <Button onClick={newAccount.onOpen} size='sm'>
-            <Plus className='size-4 mr-2' />
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="line-clamp-1 text-xl">Accounts Page</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="mr-2 size-4" />
             Add new
           </Button>
         </CardHeader>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default AccountsPage
+export default AccountsPage;
