079475808b91741fa5d9f63db557ce3100060ad7
Author <actions@github.com>
2024-05-27 18:07:12 +0000

    [ci] Format changes

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -10,7 +10,7 @@ const AccountsPage = () => {
   const newAccount = useNewAccount();
 
   return (
-    <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+    <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
           <CardTitle className="line-clamp-1 text-xl">Accounts Page</CardTitle>
