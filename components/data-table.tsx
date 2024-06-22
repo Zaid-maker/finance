b57d9716e95828283d9535c65ea99e6af61a3619
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-06-22 15:23:21 +0000

    update data-table

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -82,6 +82,10 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
         <Button
           variant="outline"
           size="sm"
